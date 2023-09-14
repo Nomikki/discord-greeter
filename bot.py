206b9a00b2e9e90a1ce50745e120d469acd2876b
Kimmo Saari <kimmo.saari@fennicagaming.com>
2023-09-14 21:47:58 +0300

    refactor: deleted language support

--- bot.py
+++ bot.py
@@ -39,6 +39,7 @@ GREETING_TIMEOUT_HOURS = int(os.getenv('GREETING_TIMEOUT_HOURS') or 2)
 GREETING_TIMEOUT = GREETING_TIMEOUT_HOURS * 60 * 60
 RULES_FILE = 'rules.yaml'
 
+FIND_URLS_FROM_MESSAGE = re.compile(r"https?://\S+")
 ALLOWED_URLS_RULES = re.compile(r"(.*)")
 ALLOWED_MIMETYPES_RULES = re.compile(r"(.*)")
 
@@ -46,20 +47,18 @@ ALLOWED_MIMETYPES_RULES = re.compile(r"(.*)")
 with open('messages.json') as f:
     messages = json.load(f)
 
-LANGUAGE = 'fi'  # Change this based on user preferences or server settings
-
 # Initialize the bot with a command prefix and description
 description = '''igorTheLoggerBot'''
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='!', case_insensitive=True, help_command=None, intents=intents)
 
-def reload_rules_if_changed(file):
+def reload_rules_if_changed():
     global rules_stamp
     global rules
     global ALLOWED_MIMETYPES_RULES
     global ALLOWED_URLS_RULES
 
-    current_stamp = os.stat(file).st_mtime
+    current_stamp = os.stat(RULES_FILE).st_mtime
     
     if rules_stamp != current_stamp:
         with open(RULES_FILE, "r") as f:
@@ -98,16 +97,16 @@ def is_mimetype_allowed(url):
         return False
 
 rules_stamp = ''
-rules = reload_rules_if_changed(RULES_FILE)
+rules = reload_rules_if_changed()
     
 @bot.event
 async def on_member_join(member):
     "This event is triggered when a new member joins the server."
     try:
         channel = await bot.fetch_channel(GREETING_CHANNEL)
-        embed = discord.Embed(title=messages[LANGUAGE]['GREETING_TITLE'], description=messages[LANGUAGE]["GREETING_MESSAGE"].format(member_mention=member.mention))
+        embed = discord.Embed(title=messages['GREETING_TITLE'], description=messages["GREETING_MESSAGE"].format(member_mention=member.mention))
         await channel.send(embed=embed)
-        await channel.send(messages[LANGUAGE]['GREETING_PROMPT'].format(member_mention=member.mention, greeting_hours=GREETING_TIMEOUT_HOURS))
+        await channel.send(messages['GREETING_PROMPT'].format(member_mention=member.mention, greeting_hours=GREETING_TIMEOUT_HOURS))
 
         def check(m):
             return bot.user.mention in m.content and m.channel == channel and member == m.author
@@ -115,12 +114,12 @@ async def on_member_join(member):
         try:
             msg = await bot.wait_for('message', check=check, timeout=GREETING_TIMEOUT)
         except asyncio.TimeoutError:
-            await channel.send(messages[LANGUAGE]["TIMEOUT_MESSAGE"])
-            await member.kick(reason=messages[LANGUAGE]["KICK_REASON"])
-            await channel.send(messages[LANGUAGE]['KICK_MESSAGE'].format(member_name=member.name))
+            await channel.send(messages["TIMEOUT_MESSAGE"])
+            await member.kick(reason=messages["KICK_REASON"])
+            await channel.send(messages['KICK_MESSAGE'].format(member_name=member.name))
         else:
             logging.info(f"Message: {msg}")
-            await channel.send(messages[LANGUAGE]["SUCCESS_MESSAGE"])
+            await channel.send(messages["SUCCESS_MESSAGE"])
     except Exception as e:
         logging.exception(f"Error in on_member_join, {e}")
 
@@ -130,10 +129,10 @@ async def on_message(message):
     "This event is triggered when a message is sent in the server."
     try:
         if message.channel.id == MEDIA_CHANNEL:
-            reload_rules_if_changed(RULES_FILE)
             attachments = [f for f in message.attachments if is_mimetype_allowed(f.url)]
-            url_pattern = r'https?://\S+'
-            urls = [url for url in re.findall(url_pattern, message.content) if is_url_allowed(url) or is_mimetype_allowed(url)]
+            urls = [url for url in FIND_URLS_FROM_MESSAGE.findall(message.content) if is_url_allowed(url) or is_mimetype_allowed(url)]
+            if (attachments or urls):
+                reload_rules_if_changed()
             if not (attachments or urls):
                 await message.delete()
     except Exception as e:
